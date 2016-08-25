b1cb5c2d011796d2492a55f8618cbfa3227d8957
rinto0503@gmail.com <anhyeuem123>
2016-08-25 20:58:28 +0700

    25/8/2016

--- src/main/java/com/jfreedom/reponsitory/ShopJPAReponsitoryCustom.java
+++ src/main/java/com/jfreedom/reponsitory/ShopJPAReponsitoryCustom.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jfreedom.model.Shop;
 
+import java.util.List;
+
 public interface ShopJPAReponsitoryCustom extends JpaRepository<Shop, Integer>{
+    List<Shop> findByName(String name);
 
 }
